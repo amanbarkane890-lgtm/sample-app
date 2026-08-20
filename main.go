14bb2c08996a7f82655eaf7c6656e10714763af2
amanbarkane890-lgtm <student-01-ce24d0ce9d9f@qwiklabs.net>
2026-08-20 16:52:14 +0000

    Fix main.go

--- main.go
+++ main.go
@@ -30,8 +30,3 @@ func redHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "image/png")
 	png.Encode(w, img)
 }
-	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
-	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{0, 0, 255, 255}}, image.ZP, draw.Src)
-	w.Header().Set("Content-Type", "image/png")
-	png.Encode(w, img)
-}
