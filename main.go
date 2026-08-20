5675a025e9eec9d6810d002a629d61039a954443
amanbarkane890-lgtm <student-01-ce24d0ce9d9f@qwiklabs.net>
2026-08-20 15:39:49 +0000

    Deploy development v2

--- main.go
+++ main.go
@@ -1,9 +1,3 @@
-/**
- * Copyright 2023 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -26,7 +20,6 @@ import (
 
 func main() {
 	http.HandleFunc("/blue", blueHandler)
-	http.HandleFunc("/red", redHandler)
 	http.HandleFunc("/red", redHandler)
 	http.ListenAndServe(":8080", nil)
 }
