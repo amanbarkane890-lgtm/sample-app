6028dadb67395c32fb4c181e80f9e30a3152d2ee
amanbarkane890-lgtm <student-01-ce24d0ce9d9f@qwiklabs.net>
2026-08-20 16:48:45 +0000

    Fix main.go

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
- *
+/*
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -23,25 +23,25 @@ func main() {
 	http.HandleFunc("/red", redHandler)
 	http.ListenAndServe(":8080", nil)
 }
-func redHandler(w http.ResponseWriter, r *http.Request) { 
-	img := image.NewRGBA(image.Rect(0, 0, 100, 100)) 
-	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{255, 0, 0, 255}}, image.ZP, draw.Src) 
+func redHandler(w http.ResponseWriter, r *http.Request) {
+	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
+	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{255, 0, 0, 255}}, image.ZP, draw.Src)
 	http.HandleFunc("/red", redHandler)
-	w.Header().Set("Content-Type", "image/png") 
-	png.Encode(w, img) 
+	w.Header().Set("Content-Type", "image/png")
+	png.Encode(w, img)
 }
-func redHandler(w http.ResponseWriter, r *http.Request) { 
-	img := image.NewRGBA(image.Rect(0, 0, 100, 100)) 
-	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{255, 0, 0, 255}}, image.ZP, draw.Src) 
-	w.Header().Set("Content-Type", "image/png") 
-	png.Encode(w, img) 
+func redHandler(w http.ResponseWriter, r *http.Request) {
+	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
+	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{255, 0, 0, 255}}, image.ZP, draw.Src)
+	w.Header().Set("Content-Type", "image/png")
+	png.Encode(w, img)
 }
 
-func redHandler(w http.ResponseWriter, r *http.Request) { 
-	img := image.NewRGBA(image.Rect(0, 0, 100, 100)) 
-	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{255, 0, 0, 255}}, image.ZP, draw.Src) 
-	w.Header().Set("Content-Type", "image/png") 
-	png.Encode(w, img) 
+func redHandler(w http.ResponseWriter, r *http.Request) {
+	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
+	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{255, 0, 0, 255}}, image.ZP, draw.Src)
+	w.Header().Set("Content-Type", "image/png")
+	png.Encode(w, img)
 }
 func blueHandler(w http.ResponseWriter, r *http.Request) {
 	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
